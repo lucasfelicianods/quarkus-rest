627c3569c4526e4ce8db5308606827f3c0951dad
gabrielcunhafatesg <gabriel.cunha@senaifatesg.com>
2021-07-06 22:36:17 -0300

    implementado delete.

--- src/main/java/org/acme/getting/started/resource/CustomerResource.java
+++ src/main/java/org/acme/getting/started/resource/CustomerResource.java
@@ -1,23 +1,22 @@
 package org.acme.getting.started.resource;
 
 import org.acme.getting.started.entity.Customer;
-import org.acme.getting.started.infrastructure.CustomerRepo;
 
-import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/customer")
 public class CustomerResource {
 
-    @Inject
-    private CustomerRepo repository;
 
     @Transactional
     @POST
@@ -32,6 +31,23 @@ public class CustomerResource {
     @Produces("application/json")
     public List<Customer> getAll() {
         return Customer.findAll().list();
+    }
+
+    @GET
+    @Path("/find")
+    public Customer findByName(@QueryParam("name") String name) {
+        return Customer.getName(name);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    @Consumes("application/json")
+    @Produces("application/json")
+    public void delete(@PathParam("id") Long id) {
+        var customerSaved = Customer.findById(id);
+        System.out.println(id);
+        Customer.deleteById(customerSaved);
     }
 
 }
